6e53bd59a8a02419daef21c528dd3cf1a588cc02
Iakinthos <ikouniaris@gmail.com>
2016-05-10 16:55:15 +0300

    Finds connections Can save connections/cities to file Can Load connections/cities(?)

--- src/main/java/Storage/Database.java
+++ src/main/java/Storage/Database.java
@@ -9,23 +9,50 @@ package Storage;
  *
  * @author Cryowynd
  */
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-public class Database {
-    
-    
-    public  void Connect(){
-        
-    
-try {
-    
-    Connection conn= DriverManager.getConnection("jdbc:mysql://10.100.51.123/test?user=it21447&password=Iakinthos");
-    System.out.println("Connecting");
 
-} catch (SQLException ex){    
-    System.out.println("SQLException: " + ex.getMessage()); System.out.println("SQLState: " +
-ex.getSQLState()); System.out.println("VendorError: " + ex.getErrorCode());
-}
-}
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Database {
+
+	public void Connect() {
+
+		System.out.println("-------- Oracle JDBC Connection Testing ------");
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your Oracle JDBC Driver?");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("Oracle JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection=DriverManager.getConnection("jdbc:oracle:thin:@10.100.51.123:1521:orcl","it214158","it214158");
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
+
 }
